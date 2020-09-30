56ebee712e563dee612b1859e038d35f76b2d82a
jamestucker1988 <xat2212@gmail.com>
2020-09-30 18:52:07 +0530

    delete unneccesary files

--- drop.js
+++
@@ -1,8 +0,0 @@
-// $(document).onload(function () {
-//   $(".dropdown").on("show.bs.dropdown", function (event) {
-//     var x = $(event.relatedTarget).text(); // Get the button text
-//     alert("You clicked on: " + x);
-//   });
-// });
-
-// $("drop-down-toggle").Click().toggle();
