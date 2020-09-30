cbe63c879549aa4b708138a76329cc2a47a6a4dc
jamestucker1988 <xat2212@gmail.com>
2020-09-30 11:44:10 +0530

    restyle the navbar, modify yml file

---
+++ drop.js
@@ -0,0 +1,8 @@
+// $(document).onload(function () {
+//   $(".dropdown").on("show.bs.dropdown", function (event) {
+//     var x = $(event.relatedTarget).text(); // Get the button text
+//     alert("You clicked on: " + x);
+//   });
+// });
+
+// $("drop-down-toggle").Click().toggle();
